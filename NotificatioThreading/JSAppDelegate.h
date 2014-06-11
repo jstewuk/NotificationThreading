7be7d1f2d91cfd2d177e3ecf49d131c2524e1882
Jim Stewart <jim@stewartstuff.com>
2014-06-11 15:02:16 -0500

    Initial Commit

---
+++ NotificatioThreading/JSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JSAppDelegate.h
+//  NotificatioThreading
+//
+//  Created by Jim on 6/11/14.
+//  Copyright (c) 2014 stewartstuff.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface JSAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ NotificatioThreading/JSAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  JSAppDelegate.m
+//  NotificatioThreading
+//
+//  Created by Jim on 6/11/14.
+//  Copyright (c) 2014 stewartstuff.com. All rights reserved.
+//
+
+#import "JSAppDelegate.h"
+
+@implementation JSAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
