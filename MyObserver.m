3ca54efdf3907b34fbb587b8d5b36a3e88fd50b9
Jim Stewart <jim@stewartstuff.com>
2014-06-12 16:33:01 -0500

    Clean up comments.

--- MyObserver.m
+++ MyObserver.m
@@ -40,20 +40,11 @@
 {
     NSLog(@"in [%@ %@]", self.className, NSStringFromSelector(_cmd));
     
-    // Executing on a thread different than the notification posting thread while the notification is being handled
-    // will cause a crash, since it starts tearing down this object before the notifications are run to completion.
+    // Will work because the notification handler is capturing self and being dispatched to a background thread.
+    // Dealloc won't get called until the handler task is finished and self is released
+    
     [[NSNotificationCenter defaultCenter] removeObserver:self];
-	  NSLog( @"dealloc %@ on thread %@", self, [NSThread currentThread] );
-    
-    // Works because removeObserver will allow all notifications on the same thread to run before finishing dealloc
-    // NB this has to be sync, since when dealloc exit the system finishes object teardown
-    // if it could be called on the notification thread need to guard against deadlock
-    
-//    dispatch_sync(dispatch_get_main_queue(), ^{
-//        NSLog(@"calling removeObserver");
-//        [[NSNotificationCenter defaultCenter] removeObserver:self];
-//        NSLog( @"removeObserver called on thread %@", [NSThread currentThread] );
-//    });
+    NSLog( @"dealloc %@ on thread %@", self, [NSThread currentThread] );
 }
 
 @end
